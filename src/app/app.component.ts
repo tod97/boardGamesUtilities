07de16bc976de00d079d4bd2c2148a0862cf7b54
Francesco <todinofrancesco97@gmail.com>
2022-10-18 20:39:23 +0200

    added timer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
+  endTime: Date;
+  remainingTime: number;
+  interval: any;
+
   constructor() {}
+
+  startTimer(seconds) {
+    this.endTime = new Date();
+    this.endTime.setTime(this.endTime.getTime() + seconds * 1000);
+    this.remainingTime = seconds;
+
+    if (this.interval) {
+      clearInterval(this.interval);
+    }
+    this.interval = setInterval(() => {
+      this.remainingTime = Math.ceil(
+        (this.endTime.getTime() - new Date().getTime()) / 1000
+      );
+      if (this.remainingTime === 0) {
+        clearInterval(this.interval);
+      }
+    }, 1000);
+  }
+
+  stopTimer() {
+    this.endTime = undefined;
+    this.remainingTime = undefined;
+    clearInterval(this.interval);
+  }
 }
